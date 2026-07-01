a06cf0624881db389321467d424f2e88f3470ef7
avycado13 <108358183+avycado13@users.noreply.github.com>
2026-06-30 17:51:37 -0700

    deploy to surge. gh pages is frickin useless

---
+++ _site/pkg/webos.d.ts
@@ -0,0 +1,42 @@
+/* tslint:disable */
+/* eslint-disable */
+
+export function wasm_start(): void;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+    readonly memory: WebAssembly.Memory;
+    readonly wasm_start: () => void;
+    readonly wasm_bindgen_7fd9c9c9710d104d___convert__closures_____invoke___web_sys_92a6ad9cc962e3b8___features__gen_KeyboardEvent__KeyboardEvent______true_: (a: number, b: number, c: any) => void;
+    readonly wasm_bindgen_7fd9c9c9710d104d___convert__closures_____invoke_______true_: (a: number, b: number) => void;
+    readonly __wbindgen_malloc: (a: number, b: number) => number;
+    readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+    readonly __wbindgen_exn_store: (a: number) => void;
+    readonly __externref_table_alloc: () => number;
+    readonly __wbindgen_externrefs: WebAssembly.Table;
+    readonly __wbindgen_destroy_closure: (a: number, b: number) => void;
+    readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+
+/**
+ * Instantiates the given `module`, which can either be bytes or
+ * a precompiled `WebAssembly.Module`.
+ *
+ * @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+ *
+ * @returns {InitOutput}
+ */
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+ * If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+ * for everything else, calls `WebAssembly.instantiate` directly.
+ *
+ * @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+ *
+ * @returns {Promise<InitOutput>}
+ */
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
